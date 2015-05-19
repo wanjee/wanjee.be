9646431bc88287f9a0e01d257af5fa6dbc52953f
Wanjee <wanjee.be@gmail.com>
2015-05-19 21:55:57 +0200

    Follow ShuweeAdminBundle installation guide (index:install & security chapters)

--- www/app/AppKernel.php
+++ www/app/AppKernel.php
@@ -16,6 +16,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Liip\ImagineBundle\LiipImagineBundle(),
+            new Wanjee\Shuwee\AdminBundle\ShuweeAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
