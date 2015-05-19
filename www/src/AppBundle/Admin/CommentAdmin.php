5291194caffa71262de2dbb37658f6daa0cf79c7
Wanjee <wanjee.be@gmail.com>
2015-05-19 22:45:29 +0200

    Follow ShuweeAdminBundle installation guide (Bundle usage) : Implement Shuwee admin classes for both Post and Comment entities; Register them as tagged services

---
+++ www/src/AppBundle/Admin/CommentAdmin.php
@@ -0,0 +1,64 @@
+<?php
+namespace AppBundle\Admin;
+
+use Wanjee\Shuwee\AdminBundle\Admin\Admin;
+use Wanjee\Shuwee\AdminBundle\Datagrid\Datagrid;
+
+/**
+ * Class PostAdmin
+ *
+ * @package AppBundle\Admin
+ */
+class CommentAdmin extends Admin
+{
+    /**
+     * Return the main admin form for this content.
+     *
+     * @return \Symfony\Component\Form\Form
+     */
+    public function getForm()
+    {
+        // Return either a fully qualified class name
+        // or the service id of your form if it is defined as a service
+        return 'AppBundle\Form\CommentType';
+    }
+
+    /**
+     * @return Datagrid
+     */
+    public function getDatagrid()
+    {
+        $datagrid = new Datagrid($this);
+
+        $datagrid
+            ->addField('id', 'text')
+            ->addField('content', 'text')
+            ->addField('authorEmail', 'text');
+
+        return $datagrid;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEntityName()
+    {
+        return 'AppBundle:Comment';
+    }
+
+    /**
+     * @return string
+     */
+    public function getEntityClass()
+    {
+        return 'AppBundle\Entity\Comment';
+    }
+
+    /**
+     * @return string
+     */
+    public function getLabel()
+    {
+        return '{0} Comments|{1} Comment|]1,Inf] Comments';
+    }
+}
---
+++ www/src/AppBundle/Admin/PostAdmin.php
@@ -0,0 +1,63 @@
+<?php
+namespace AppBundle\Admin;
+
+use Wanjee\Shuwee\AdminBundle\Admin\Admin;
+use Wanjee\Shuwee\AdminBundle\Datagrid\Datagrid;
+
+/**
+ * Class PostAdmin
+ *
+ * @package AppBundle\Admin
+ */
+class PostAdmin extends Admin
+{
+    /**
+     * Return the main admin form for this content.
+     *
+     * @return \Symfony\Component\Form\Form
+     */
+    public function getForm()
+    {
+        // Return either a fully qualified class name
+        // or the service id of your form if it is defined as a service
+        return 'AppBundle\Form\PostType';
+    }
+
+    /**
+     * @return Datagrid
+     */
+    public function getDatagrid()
+    {
+        $datagrid = new Datagrid($this);
+
+        $datagrid
+            ->addField('id', 'text')
+            ->addField('title', 'text');
+
+        return $datagrid;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEntityName()
+    {
+        return 'AppBundle:Post';
+    }
+
+    /**
+     * @return string
+     */
+    public function getEntityClass()
+    {
+        return 'AppBundle\Entity\Post';
+    }
+
+    /**
+     * @return string
+     */
+    public function getLabel()
+    {
+        return '{0} Posts|{1} Post|]1,Inf] Posts';
+    }
+}
