058724a7ab177ecc942dc5c02813808ff52c631a
Wanjee <wanjee.be@gmail.com>
2015-05-21 21:58:55 +0200

    Add image upload to Post entity (TODO replace with a more optimal upload solution)

--- www/src/AppBundle/Entity/Post.php
+++ www/src/AppBundle/Entity/Post.php
@@ -4,6 +4,7 @@ namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
@@ -12,6 +13,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  *
  * @ORM\Table(name="post")
  * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
  */
 class Post
 {
@@ -44,6 +46,10 @@ class Post
      */
     private $content;
     /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $image;
+    /**
      * @ORM\Column(type="string")
      * @Assert\Email()
      */
@@ -70,6 +76,26 @@ class Post
      * @ORM\OrderBy({"publishedAt" = "DESC"})
      */
     private $comments;
+
+    /**
+     * Not mapped, used for $this->image upload
+     */
+    private $temp;
+
+    /**
+     * Not mapped, used for $this->image upload
+     * @Assert\File(
+     *     maxSize="3M",
+     *     mimeTypes = {"image/png", "image/jpeg"},
+     * )
+     */
+    private $file;
+
+    /**
+     * Not mapped, used for $this->image upload
+     * @var string
+     */
+    protected $path = 'posts';
 
     public function __construct()
     {
@@ -110,6 +136,117 @@ class Post
     public function setContent($content)
     {
         $this->content = $content;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param mixed $image
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+    /**
+     * Set path
+     *
+     * @param string $path
+     * @return Offer
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    /**
+     * Get path
+     *
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * Sets file.
+     *
+     * @param UploadedFile $file
+     */
+    public function setFile(UploadedFile $file = null)
+    {
+        $this->file = $file;
+        // check if we have an old image path
+        if (isset($this->image)) {
+            // store the old name to delete after the update
+            $this->temp = $this->image;
+            $this->image = null;
+        }
+    }
+
+    /**
+     * Get file.
+     *
+     * @return UploadedFile
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function preUpload()
+    {
+        if (null !== $this->getFile()) {
+            $this->image = $this->uniqueCleanFilename();
+        }
+    }
+
+    /**
+     * @ORM\PostPersist()
+     * @ORM\PostUpdate()
+     */
+    public function upload()
+    {
+        if (null === $this->getFile()) {
+            return;
+        }
+
+        // if there is an error when moving the file, an exception will
+        // be automatically thrown by move(). This will properly prevent
+        // the entity from being persisted to the database on error
+        $this->getFile()->move($this->getUploadRootDir(), $this->image);
+
+        // check if we have an old image
+        if (!empty($this->temp)) {
+            // delete the old image
+            unlink($this->getUploadRootDir().'/'.$this->temp);
+            // clear the temp image path
+            $this->temp = null;
+        }
+        $this->file = null;
+    }
+
+    /**
+     * @ORM\PostRemove()
+     */
+    public function removeUpload()
+    {
+        $file = $this->getAbsolutePath();
+        if ($file) {
+            unlink($file);
+        }
     }
 
     public function getAuthorEmail()
@@ -173,6 +310,404 @@ class Post
     public function setStatus($status)
     {
         $this->status = $status;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getAbsolutePath()
+    {
+        return null === $this->image
+            ? null
+            : $this->getUploadRootDir().'/'.$this->image;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getWebPath()
+    {
+        return null === $this->image
+            ? null
+            : $this->getUploadDir().'/'.$this->image;
+    }
+
+    /**
+     * @return string
+     */
+    protected function getUploadRootDir()
+    {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return __DIR__.'/../../../web/'.$this->getUploadDir();
+    }
+
+    /**
+     * @return string
+     */
+    protected function getUploadDir()
+    {
+        // get rid of the __DIR__ so it doesn't screw up
+        // when displaying uploaded doc/image in the view.
+        return 'uploads/' . $this->getPath();
+    }
+
+    /**
+     * Helper to get a unique clean file name
+     */
+    private function uniqueCleanFilename()
+    {
+        $name = $this->getFile()->getClientOriginalName();
+
+        // remove extension
+        $name = pathinfo($name, PATHINFO_FILENAME);
+
+        // clean name
+        $options = array(
+            'delimiter' => '_',
+            'transliterate' => true,
+        );
+        $name = $this->sanitize($name, $options);
+
+        // ensure it is unique
+        $i = 0;
+        $try = $this->getUploadRootDir() . '/' . $name . '.' . $this->getFile()->guessExtension();
+        while (file_exists($try)) {
+            $i++;
+            $try = $this->getUploadRootDir() . '/' . $name . '_' . $i . '.' . $this->getFile()->guessExtension();
+        }
+
+        if ($i > 0) {
+            $name = $name . '_' . $i;
+        }
+
+        // add extension back, depending on mime type
+        $name = $name . '.' . $this->getFile()->guessExtension();
+
+        return $name;
+    }
+
+    /**
+     * Helper to sanitize a filename
+     */
+    private function sanitize($str, $options = array())
+    {
+        // Make sure string is in UTF-8 and strip invalid UTF-8 characters
+        $str = mb_convert_encoding((string) $str, 'UTF-8', mb_list_encodings());
+        $defaults = array(
+            'delimiter' => '-',
+            'limit' => null,
+            'lowercase' => true,
+            'transliterate' => false,
+        );
+        // Merge options
+        $options = array_merge($defaults, $options);
+        $char_map = array(
+            // Latin
+            'À' => 'A',
+            'Á' => 'A',
+            'Â' => 'A',
+            'Ã' => 'A',
+            'Ä' => 'A',
+            'Å' => 'A',
+            'Æ' => 'AE',
+            'Ç' => 'C',
+            'È' => 'E',
+            'É' => 'E',
+            'Ê' => 'E',
+            'Ë' => 'E',
+            'Ì' => 'I',
+            'Í' => 'I',
+            'Î' => 'I',
+            'Ï' => 'I',
+            'Ð' => 'D',
+            'Ñ' => 'N',
+            'Ò' => 'O',
+            'Ó' => 'O',
+            'Ô' => 'O',
+            'Õ' => 'O',
+            'Ö' => 'O',
+            'Ő' => 'O',
+            'Ø' => 'O',
+            'Ù' => 'U',
+            'Ú' => 'U',
+            'Û' => 'U',
+            'Ü' => 'U',
+            'Ű' => 'U',
+            'Ý' => 'Y',
+            'Þ' => 'TH',
+            'ß' => 'ss',
+            'à' => 'a',
+            'á' => 'a',
+            'â' => 'a',
+            'ã' => 'a',
+            'ä' => 'a',
+            'å' => 'a',
+            'æ' => 'ae',
+            'ç' => 'c',
+            'è' => 'e',
+            'é' => 'e',
+            'ê' => 'e',
+            'ë' => 'e',
+            'ì' => 'i',
+            'í' => 'i',
+            'î' => 'i',
+            'ï' => 'i',
+            'ð' => 'd',
+            'ñ' => 'n',
+            'ò' => 'o',
+            'ó' => 'o',
+            'ô' => 'o',
+            'õ' => 'o',
+            'ö' => 'o',
+            'ő' => 'o',
+            'ø' => 'o',
+            'ù' => 'u',
+            'ú' => 'u',
+            'û' => 'u',
+            'ü' => 'u',
+            'ű' => 'u',
+            'ý' => 'y',
+            'þ' => 'th',
+            'ÿ' => 'y',
+            // Latin symbols
+            '©' => '(c)',
+            // Greek
+            'Α' => 'A',
+            'Β' => 'B',
+            'Γ' => 'G',
+            'Δ' => 'D',
+            'Ε' => 'E',
+            'Ζ' => 'Z',
+            'Η' => 'H',
+            'Θ' => '8',
+            'Ι' => 'I',
+            'Κ' => 'K',
+            'Λ' => 'L',
+            'Μ' => 'M',
+            'Ν' => 'N',
+            'Ξ' => '3',
+            'Ο' => 'O',
+            'Π' => 'P',
+            'Ρ' => 'R',
+            'Σ' => 'S',
+            'Τ' => 'T',
+            'Υ' => 'Y',
+            'Φ' => 'F',
+            'Χ' => 'X',
+            'Ψ' => 'PS',
+            'Ω' => 'W',
+            'Ά' => 'A',
+            'Έ' => 'E',
+            'Ί' => 'I',
+            'Ό' => 'O',
+            'Ύ' => 'Y',
+            'Ή' => 'H',
+            'Ώ' => 'W',
+            'Ϊ' => 'I',
+            'Ϋ' => 'Y',
+            'α' => 'a',
+            'β' => 'b',
+            'γ' => 'g',
+            'δ' => 'd',
+            'ε' => 'e',
+            'ζ' => 'z',
+            'η' => 'h',
+            'θ' => '8',
+            'ι' => 'i',
+            'κ' => 'k',
+            'λ' => 'l',
+            'μ' => 'm',
+            'ν' => 'n',
+            'ξ' => '3',
+            'ο' => 'o',
+            'π' => 'p',
+            'ρ' => 'r',
+            'σ' => 's',
+            'τ' => 't',
+            'υ' => 'y',
+            'φ' => 'f',
+            'χ' => 'x',
+            'ψ' => 'ps',
+            'ω' => 'w',
+            'ά' => 'a',
+            'έ' => 'e',
+            'ί' => 'i',
+            'ό' => 'o',
+            'ύ' => 'y',
+            'ή' => 'h',
+            'ώ' => 'w',
+            'ς' => 's',
+            'ϊ' => 'i',
+            'ΰ' => 'y',
+            'ϋ' => 'y',
+            'ΐ' => 'i',
+            // Turkish
+            'Ş' => 'S',
+            'İ' => 'I',
+            'Ç' => 'C',
+            'Ü' => 'U',
+            'Ö' => 'O',
+            'Ğ' => 'G',
+            'ş' => 's',
+            'ı' => 'i',
+            'ç' => 'c',
+            'ü' => 'u',
+            'ö' => 'o',
+            'ğ' => 'g',
+            // Russian
+            'А' => 'A',
+            'Б' => 'B',
+            'В' => 'V',
+            'Г' => 'G',
+            'Д' => 'D',
+            'Е' => 'E',
+            'Ё' => 'Yo',
+            'Ж' => 'Zh',
+            'З' => 'Z',
+            'И' => 'I',
+            'Й' => 'J',
+            'К' => 'K',
+            'Л' => 'L',
+            'М' => 'M',
+            'Н' => 'N',
+            'О' => 'O',
+            'П' => 'P',
+            'Р' => 'R',
+            'С' => 'S',
+            'Т' => 'T',
+            'У' => 'U',
+            'Ф' => 'F',
+            'Х' => 'H',
+            'Ц' => 'C',
+            'Ч' => 'Ch',
+            'Ш' => 'Sh',
+            'Щ' => 'Sh',
+            'Ъ' => '',
+            'Ы' => 'Y',
+            'Ь' => '',
+            'Э' => 'E',
+            'Ю' => 'Yu',
+            'Я' => 'Ya',
+            'а' => 'a',
+            'б' => 'b',
+            'в' => 'v',
+            'г' => 'g',
+            'д' => 'd',
+            'е' => 'e',
+            'ё' => 'yo',
+            'ж' => 'zh',
+            'з' => 'z',
+            'и' => 'i',
+            'й' => 'j',
+            'к' => 'k',
+            'л' => 'l',
+            'м' => 'm',
+            'н' => 'n',
+            'о' => 'o',
+            'п' => 'p',
+            'р' => 'r',
+            'с' => 's',
+            'т' => 't',
+            'у' => 'u',
+            'ф' => 'f',
+            'х' => 'h',
+            'ц' => 'c',
+            'ч' => 'ch',
+            'ш' => 'sh',
+            'щ' => 'sh',
+            'ъ' => '',
+            'ы' => 'y',
+            'ь' => '',
+            'э' => 'e',
+            'ю' => 'yu',
+            'я' => 'ya',
+            // Ukrainian
+            'Є' => 'Ye',
+            'І' => 'I',
+            'Ї' => 'Yi',
+            'Ґ' => 'G',
+            'є' => 'ye',
+            'і' => 'i',
+            'ї' => 'yi',
+            'ґ' => 'g',
+            // Czech
+            'Č' => 'C',
+            'Ď' => 'D',
+            'Ě' => 'E',
+            'Ň' => 'N',
+            'Ř' => 'R',
+            'Š' => 'S',
+            'Ť' => 'T',
+            'Ů' => 'U',
+            'Ž' => 'Z',
+            'č' => 'c',
+            'ď' => 'd',
+            'ě' => 'e',
+            'ň' => 'n',
+            'ř' => 'r',
+            'š' => 's',
+            'ť' => 't',
+            'ů' => 'u',
+            'ž' => 'z',
+            // Polish
+            'Ą' => 'A',
+            'Ć' => 'C',
+            'Ę' => 'e',
+            'Ł' => 'L',
+            'Ń' => 'N',
+            'Ó' => 'o',
+            'Ś' => 'S',
+            'Ź' => 'Z',
+            'Ż' => 'Z',
+            'ą' => 'a',
+            'ć' => 'c',
+            'ę' => 'e',
+            'ł' => 'l',
+            'ń' => 'n',
+            'ó' => 'o',
+            'ś' => 's',
+            'ź' => 'z',
+            'ż' => 'z',
+            // Latvian
+            'Ā' => 'A',
+            'Č' => 'C',
+            'Ē' => 'E',
+            'Ģ' => 'G',
+            'Ī' => 'i',
+            'Ķ' => 'k',
+            'Ļ' => 'L',
+            'Ņ' => 'N',
+            'Š' => 'S',
+            'Ū' => 'u',
+            'Ž' => 'Z',
+            'ā' => 'a',
+            'č' => 'c',
+            'ē' => 'e',
+            'ģ' => 'g',
+            'ī' => 'i',
+            'ķ' => 'k',
+            'ļ' => 'l',
+            'ņ' => 'n',
+            'š' => 's',
+            'ū' => 'u',
+            'ž' => 'z'
+        );
+
+        // Transliterate characters to ASCII
+        if ($options['transliterate']) {
+            $str = str_replace(array_keys($char_map), $char_map, $str);
+        }
+        // Replace non-alphanumeric characters with our delimiter
+        $str = preg_replace('/[^\p{L}\p{Nd}]+/u', $options['delimiter'], $str);
+        // Remove duplicate delimiters
+        $str = preg_replace('/(' . preg_quote($options['delimiter'], '/') . '){2,}/', '$1', $str);
+        // Truncate slug to max. characters
+        $str = mb_substr($str, 0, ($options['limit'] ? $options['limit'] : mb_strlen($str, 'UTF-8')), 'UTF-8');
+        // Remove delimiter from ends
+        $str = trim($str, $options['delimiter']);
+
+        return $options['lowercase'] ? mb_strtolower($str, 'UTF-8') : $str;
     }
 
 }
