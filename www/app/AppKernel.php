7d159d47af5539eea4b1d1e55babc254eabb06f4
Wanjee <wanjee.be@gmail.com>
2015-06-23 22:50:45 +0200

    Install new ShuweeAdminBundle dependencies

--- www/app/AppKernel.php
+++ www/app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new Liip\ImagineBundle\LiipImagineBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Wanjee\Shuwee\AdminBundle\ShuweeAdminBundle(),
