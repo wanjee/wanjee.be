c178bdf2b0d1b36ff09021d86622203343ba510f
Wanjee <wanjee.be@gmail.com>
2015-10-03 21:36:58 +0200

    Add gruntfile

---
+++ front/Gruntfile.js
@@ -0,0 +1,107 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.initConfig({
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1,
+                sourceMap: true,
+                // Disable merge of properties and selectors
+                advanced: false
+            },
+            app: {
+                files: {
+                    'web/css/app.min.css': [
+                        'web/css/app/styles.css'
+                    ]
+                }
+            },
+            vendors: {
+                files: {
+                    'web/css/vendors.min.css': [
+                        // do not use *.js as order is important
+                        'web/css/vendors/bootstrap.min.css'
+                    ]
+                }
+            }
+        },
+        compass: {
+            app: {
+                options: {
+                    sassDir: 'web/scss',
+                    cssDir: 'web/css/app',
+                    environment: 'development',
+                    noLineComments: true,
+                    outputStyle: 'nested',
+                    sourcemap: true,
+                    imagesDir: 'web/img',
+                    http_path: 'web',
+                    relativeAssets: true
+                }
+            }
+        },
+        jshint: {
+            app: {
+                src: ['web/js/app/*.js']
+            }
+        },
+        uglify: {
+            options: {
+                mangle: false,
+                sourcemap: true
+            },
+            app: {
+                files: {
+                    'web/js/app.min.js': [
+                        'web/js/app/main.js'
+                    ]
+                }
+            },
+            vendors: {
+                files: {
+                    'web/js/vendors.min.js': [
+                        'web/js/vendors/jquery.min.js',
+                        'web/js/vendors/underscore.min.js',
+                        'web/js/vendors/backbone.min.js',
+                        'web/js/vendors/bootstrap.min.js'
+                    ]
+                }
+            }
+        },
+        'watch': {
+            styles: {
+                files: 'web/scss/**/*.scss',
+                tasks: ['compass','cssmin:app'],
+                options: {
+                    debounceDelay: 250
+                }
+            },
+            scripts: {
+                files: 'web/js/app/**',
+                tasks: ['jshint', 'uglify:app'],
+                options: {
+                    interrupt: true
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('default', [
+        'dist',
+        'watch'
+    ]);
+
+    grunt.registerTask('dist', [
+        'compass',
+        'cssmin',
+        'uglify'
+    ]);
+};
