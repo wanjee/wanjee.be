d0effa2bfb7daaa470bf3bfa8d5e875e5bdb4ac2
Wanjee <wanjee.be@gmail.com>
2015-11-12 21:21:22 +0100

    Avoid firing smooth scroll on backbone links

--- www/web/js/app/effects.js
+++ www/web/js/app/effects.js
@@ -59,7 +59,7 @@ function hasScrolled() {
 /**
  * Smooth scrolling
  */
-$('a[href*=#]:not([href=#])').click(function() {
+$('a[href*=#]:not([href=#]):not([href^="#/"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
 
         var target = $(this.hash);
