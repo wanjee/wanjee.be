34ec7baa89fba82e4e2af1996438bcb18d6a52fb
Sébastien Piraux <spi@emakina.com>
2017-01-06 16:58:59 +0100

    Implement UserAdmin

---
+++ src/AppBundle/Admin/ShuweeUserAdmin.php
@@ -0,0 +1,14 @@
+<?php
+namespace AppBundle\Admin;
+
+use Wanjee\Shuwee\AdminBundle\Admin\AbstractUserAdmin;
+
+/**
+ * Class ShuweeUserAdmin
+ *
+ * @package AppBundle\Admin
+ */
+class ShuweeUserAdmin extends AbstractUserAdmin
+{
+    // Be lazy, let AbstractUserAdmin do all the job
+}
